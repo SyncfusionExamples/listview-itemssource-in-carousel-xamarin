b17d22ce570a044aadfb1d1c8e297eb2142ef6b6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-24 11:53:03 +0530

    XAMARIN-27211 - Sample and screen shot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/PagesViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class PagesViewModel
+    {
+        public ObservableCollection<List<Contacts>> CarouselPages { get; set; }
+
+        public PagesViewModel(double height)
+        {
+            int j = 0;
+            int itemsPerPage = (int)(height / 100) * 3;
+            int temp = itemsPerPage;
+            CarouselPages = new ObservableCollection<List<Contacts>>();
+            var contactsviewmodel = new ContactsViewModel();
+          
+            var pageCount = Math.Ceiling((double)contactsviewmodel.ContactsInfo.Count / itemsPerPage);
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                var source = contactsviewmodel.ContactsInfo.Skip(j).Take(temp);
+                var items = source.AsEnumerable().ToList();
+                CarouselPages.Add(items);
+                j += itemsPerPage;
+            }
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Views/ListViewCarousel.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewCarousel : CarouselPage
+    {
+        PagesViewModel pagesViewModel;
+
+        public ListViewCarousel()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            if (height > 0 && BindingContext == null)
+            {
+                pagesViewModel = new PagesViewModel(height);
+                BindingContext = pagesViewModel;
+                ItemsSource = pagesViewModel.CarouselPages;
+            }
+            base.OnSizeAllocated(width, height);
+        }
+    }
+}
